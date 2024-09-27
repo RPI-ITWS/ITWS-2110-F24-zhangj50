554a3a389b5fd6add4165e5f6cae3793c758ca35
Justin Zhang <justinzwork23@gmail.com>
2024-09-27 03:17:15 -0400

    finished the css

--- Weather/app.js
+++ Weather/app.js
@@ -1,14 +1,25 @@
-const API_KEY = "2647e6e2577161f590f6002bb89571cb";
-async function currentAPI(lat, lon) {
-  const request = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`
-  );
-  const response = request.json();
-  response.then((data) => {
-    console.log(data);
+window.addEventListener("load", function () {
+  const API_KEY = "2647e6e2577161f590f6002bb89571cb";
+  async function currentAPI(lat, lon) {
+    const request = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`
+    );
+    const response = request.json();
+    response.then((data) => {
+      console.log(data);
+    });
+  }
+  async function forecastAPI(lat, lon) {
+    const request = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`
+    );
+    const response = request.json();
+    response.then((data) => {
+      console.log(data);
+    });
+  }
+  navigator.geolocation.getCurrentPosition((position) => {
+    currentAPI(position.coords.latitude, position.coords.longitude);
+    forecastAPI(position.coords.latitude, position.coords.longitude);
   });
-}
-
-navigator.geolocation.getCurrentPosition((position) => {
-  currentAPI(position.coords.latitude, position.coords.longitude);
 });
