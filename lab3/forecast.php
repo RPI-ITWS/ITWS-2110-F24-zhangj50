824ce3001a7fabb23e8dd8c2c429ab7c2cbe5f1c
Justin Zhang <justinzwork23@gmail.com>
2024-10-04 00:52:12 -0400

    fixed receiving body data from ajax call

--- lab3/forecast.php
+++ lab3/forecast.php
@@ -12,24 +12,11 @@
     }
     $data = file_get_contents('php://input');
 
-// Output the received data for debugging
-error_log("Received data: " . $data);
 
-// Convert JSON string to PHP object/array
-$jsonData = json_decode($data, true);
-
-// Debug output
-error_log(print_r($jsonData, true)); // Log the decoded data
-
-// Respond back with the data (for testing purposes)
-echo json_encode($jsonData);
-    $body = [
-      'content' => $_POST
-    ];
     $query = 'update weather set data=(?) where api = "forecast"';
     $statement = $db->prepare($query);
          // bind our variable to the question mark
-   $statement->bind_param("s", $_POST["body"]);
+   $statement->bind_param("s", $data);
    // make it so:
    $statement->execute();
    $statement->close();
