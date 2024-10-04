270eb5849f3b541d64f96da3582c5fce3ec61eb1
Justin Zhang <justinzwork23@gmail.com>
2024-10-04 03:19:30 -0400

    Update updateTemp.php

--- lab3/updateTemp.php
+++ lab3/updateTemp.php
@@ -16,11 +16,13 @@
     $result = $statement->get_result();
     $row = $result->fetch_assoc();
     $data = $row["data"];
+    
    $num  = file_get_contents('php://input');
    $jsonData = json_decode($data, true);
    $jsonNum = json_decode($num, true);
    $jsonData["main"]["temp"] = $jsonNum["num"];
-   $changedEncode = json_decode($jsonData, true);
+   $changedEncode = json_encode($jsonData, true);
+
    $query = 'update weather set data=(?) where api = "current"';
    $statement = $db->prepare($query);
    $statement->bind_param("s", $changedEncode);
